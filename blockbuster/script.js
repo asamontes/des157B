07a871398105bd2829a4c1ce59cc72137d0280dd
asamontes <asamontes@ucdavis.edu>
2024-04-19 01:00:44 -0700

    updates

--- blockbuster/script.js
+++ blockbuster/script.js
@@ -1,5 +1,6 @@
 (function(){
     'use strict'
+    console.log('reading');
     const fs = document.querySelector('.fa-fish');     
     
     fs.addEventListener('click', function() {
@@ -10,53 +11,77 @@
         }
     });
 
-    console.log('reading');
-            const myVideo = document.querySelector('#myVideo');
-            const line1 = document.querySelector('#line1');
-            const line2 = document.querySelector('#line2');
-            const line3 = document.querySelector('#line3');
-            const line4 = document.querySelector('#line4');
-            const line5 = document.querySelector('#line5');
+    const myVideo = document.querySelector('#myVideo');
+    const myVideo2 = document.querySelector('#myVideo2');
+    const line1 = document.querySelector('#line1');
+    const line2 = document.querySelector('#line2');
+    const line3 = document.querySelector('#line3');
+    const line4 = document.querySelector('#line4');
+    const line5 = document.querySelector('#line5');
+    
+    const poem = {
+        start: [0, 5, 8, 11, 14],
+        stop: [4, 7, 10, 13, 16],
+        line: [line1, line2, line3, line4, line5]
+    };
+    
+    const intervalID = setInterval(checkTime, 1000);
+    
+    function checkTime() {
+        console.log(parseInt(myVideo.currentTime));
 
-            const poem = {
-                start: [0, 7, 15, 22, 30],
-                stop: [4, 11, 19, 26, 35],
-                line: [line1, line2, line3, line4, line5]
-            }
+        if (poem.start[0] < myVideo.currentTime && myVideo.currentTime < poem.stop[0]) {
+            poem.line[0].className = "showing";
+          } else {
+            poem.line[0].className = "hidden";
+          }
+          if (poem.start[1] < myVideo.currentTime && myVideo.currentTime < poem.stop[1]) {
+            poem.line[1].className = "showing";
+          } else {
+            poem.line[1].className = "hidden";
+          }
+          if (poem.start[2] < myVideo.currentTime && myVideo.currentTime < poem.stop[2]) {
+            poem.line[2].className = "showing";
+          } else {
+            poem.line[2].className = "hidden";
+          }
+          if (poem.start[3] < myVideo.currentTime && myVideo.currentTime < poem.stop[3]) {
+            poem.line[3].className = "showing";
+          } else {
+            poem.line[3].className = "hidden";
+          }
+          if (poem.start[4] < myVideo.currentTime && myVideo.currentTime < poem.stop[4]) {
+            poem.line[4].className = "showing";
+          } else {
+            poem.line[4].className = "hidden";
+          }
+          if (poem.start[5] < myVideo.currentTime && myVideo.currentTime < poem.stop[2]) {
+            poem.line[5].className = "showing";
+          } else {
+            poem.line[5].className = "hidden";
+          }
+        
+        // for (let i = 0; i < poem.start.length; i++) {
+        //     if (poem.start[i] < myVideo.currentTime && myVideo.currentTime < poem.stop[i]) {
+        //         poem.line[i].className = "showing";
+        //     } else {
+        //         poem.line[i].className = "hidden";
+        //     }
+        // };
+    }
+    
 
-            const intervalID = setInterval(checkTime, 1000);
+    const daCircle = document.querySelector('.image');
 
-            function checkTime() {
-                console.log(parseInt(myVideo.currentTime));
+    daCircle.addEventListener('mousedown', function(event){
+        console.log(event.clientX);
+        myVideo.style.display = 'none';
+        myVideo2.style.display = 'block';
+    })
 
-                // if (poem.start[0] < myVideo.currentTime && myVideo.currentTime < poem.stop[0]) {
-                //     poem.line[0].className = "showing";
-                // } else {
-                //     line1.className = "hidden";
-                // }
-                // if (5 < myVideo.currentTime && myVideo.currentTime < 7) {
-                //     line2.className = "showing";
-                // } else {
-                //     line2.className = "hidden";
-                // }
-
-                // if (8 < myVideo.currentTime && myVideo.currentTime < 10) {
-                //     line3.className = "showing";
-                // } else {
-                //     line3.className = "hidden";
-                // }
-
-                for (let i = 0; i < poem.start.length; i++) {
-                    if (poem.start[i] < myVideo.currentTime && myVideo.currentTime < poem.stop[i]) {
-                        poem.line[i].className = "showing";
-                    } else {
-                        poem.line[i].className = "hidden";
-                    }
-                };
-            }
-
-            // myVideo.addEventListener('mousemove', function(event) {
-            //     console.log(event.clientX);
-            //     myVideo.playbackRate = event.clientX / 300 + .25;
-            //   })
+    daCircle.addEventListener('mouseup', function(event){
+        console.log(event.clientx);
+        myVideo.style.display = 'block';
+        myVideo2.style.display = 'none';
+    })
 })();
